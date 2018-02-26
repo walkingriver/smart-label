2c24dda9b773648e7b5464e4b6b9202248b09bbc
Mike Callaghan <mikecallaghan67@gmail.com>
2018-02-26 16:19:56 -0500

    Customize homep age

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -10,19 +10,32 @@ export class AppHome {
   render() {
     return (
       <div class='app-home'>
+      <h2>What is it?</h2>
         <p>
-          Welcome to the Stencil App Starter.
-          You can use this starter to build entire apps all with
-          web components using Stencil!
-          Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
+          This is a custom web component written in Stencil JS that implements
+          a `smart` text label. Use it anywhere you'd use a &lt;span&gt; element
+          to provide the following custom functionality:
+        </p>
+        <ul>
+          <li>Automatically trim whitespace from strings.</li>
+          <li>Provide a copy-to-clipboard link for each element, displayed on
+            hover.</li>
+        </ul>
+
+        <p>
+          Check out the docs on <a href='https://stenciljs.com'>stenciljs.com</a>
+          for more information.
         </p>
 
-        <stencil-route-link url='/profile/stencil'>
-          <button>
-            Profile page
-          </button>
-        </stencil-route-link>
+        <h2>Usage</h2>
+        &lt;smart-label alt-text="Copy" text="    Your Text, with or without whitespace   "&gt;
+        &lt;/smart-label&gt;
 
+        <hr />
+        <h2>Test here</h2>
+        <p>Hover your mouse over each of the smart labels below. Click the
+          copy icon that appears to copy just that text to the clipboard.
+        </p>
         <smart-label alt-text="Copy" text="    Mike was Here   "></smart-label>
         <smart-label alt-text="Copy" text="    Mike was There   "></smart-label>
         <smart-label alt-text="Copy" text="    Mike was Everywhere   "></smart-label>
